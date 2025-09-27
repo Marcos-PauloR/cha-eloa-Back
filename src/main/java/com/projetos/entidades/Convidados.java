d4aaa38bccf934b18ba454cab12d12e17bad9dbb
Marcos Paulo R <58742152+Marcos-PauloR@users.noreply.github.com>
2025-09-27 18:01:44 -0300

    alterado acompanhantes

--- src/main/java/com/projetos/entidades/Convidados.java
+++ src/main/java/com/projetos/entidades/Convidados.java
@@ -16,18 +16,20 @@ public class Convidados {
     private String nome;
     @Column(name = "mensagem", columnDefinition = "TEXT")
     private String mensagem;
-    private List<String>  acompanhates;
+    private List<String> companheiros;
+    private Integer acompanhates;
     private String presente;
     private boolean confirmado;
 
-    public Convidados(Integer id, String nome, String mensagem, List<String> acompanhates, String presente,
-                      boolean confirmado) {
+    public Convidados(Integer id, String nome, String mensagem, Integer acompanhates, String presente,
+                      boolean confirmado, List<String>companheiros) {
         this.id           = id;
         this.nome         = nome;
         this.mensagem     = mensagem;
         this.acompanhates = acompanhates;
         this.presente     = presente;
         this.confirmado   = confirmado;
+        this.companheiros = companheiros;
     }
 
     public Convidados() {
